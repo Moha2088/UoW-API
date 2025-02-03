44ac9884a01ba72177fe302a88a7363a6fec188a
Moha2088 <113173091+Moha2088@users.noreply.github.com>
2025-02-03 21:12:15 +0100

    add repository and add seeddata

--- UoW-API/Program.cs
+++ UoW-API/Program.cs
@@ -3,6 +3,8 @@
 using UoW_API.Repositories.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.SqlServer;
+using UoW_API.Repositories.Entities.;
+using UoW_API.Repositories.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +14,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DBConnection") ?? throw new InvalidOperationException("ConnectionString 'DBConnection' not found!")));
-
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -25,6 +26,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+#region Seeder
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+Seeder.Seed(services);
+
+#endregion
 
 app.UseHttpsRedirection();
 
